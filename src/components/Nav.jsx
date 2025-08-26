b3ab2348a82e95334e0e083dbc91f3fb4f0cf761
Njugunaaa <joshuangich2@gmail.com>
2025-08-26 12:06:10 +0300

    Initial commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import logo from "../assets/logo.svg";
+import logo from "../assets/014.png";
 import { Search, X, Menu } from "lucide-react";
 
 function Nav() {
